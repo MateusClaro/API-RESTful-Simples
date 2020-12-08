5fbf94edbafaa9289eefa7a1f6b555f370b0f705
Mateus Filipe Claro <74942467+MateusClaro@users.noreply.github.com>
2020-12-07 23:12:00 -0300

    Add files via upload

--- src/main/java/com/carros/api/CarrosController.java
+++ src/main/java/com/carros/api/CarrosController.java
@@ -1,36 +1,49 @@
 package com.carros.api;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.Optional;
+import java.util.*;
 
 
 import com.carros.domain.Carro;
 import com.carros.domain.CarroService;
+import com.carros.domain.dto.CarroDTO;
 @RestController
 @RequestMapping("/api/v1/carros")
 public class CarrosController {
 	@Autowired
 	private CarroService service;	
-	
+
 	@GetMapping()
-	public Iterable<Carro> get() {
-		return service.getCarros();
+	public ResponseEntity get() {
+		return ResponseEntity.ok(service.getCarros());
 	}
 	
 	@GetMapping("/{id}")
-	public Optional<Carro> get(@PathVariable("id") Long id) {
-		return service.getCarroById(id);
+	public ResponseEntity get(@PathVariable("id") Long id) {
+		Optional<Carro> carro = service.getCarroById(id);
+		
+		return carro
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
 	}
 
-	@GetMapping("/tipo/{tipo}")
-	public Iterable<Carro> getCarrosByTipo(@PathVariable("tipo") String tipo) {
-		return service.getCarrosByTipo(tipo);
+	@GetMapping("/tipo/{tipo}")	
+	public ResponseEntity getCarrosByTipo(@PathVariable("tipo") String tipo) {
+		List<CarroDTO> carros = service.getCarrosByTipo(tipo);
+		
+		return carros.isEmpty() ?
+				ResponseEntity.noContent().build() :
+					ResponseEntity.ok(carros);	
 	}
 	
 	@PostMapping
@@ -39,4 +52,25 @@ public class CarrosController {
 		
 		return "Carro salvo com sucesso: " + c.getId();
 	}
-}
+	
+	@PutMapping("/{id}")
+	public String put(@PathVariable("id") Long id, @RequestBody Carro carro) {
+		
+		Carro c = service.update(carro, id);
+		
+		return "Carro atualizado com sucesso" + c.getId();
+	}
+	
+	@DeleteMapping("/{id}")
+	public String delete(@PathVariable("id") Long id) {
+		
+		service.delete(id);
+		
+		return "Carro deletado com sucesso";
+	}
+	
+}	
+	
+	
+
+
--- src/main/java/com/carros/domain/CarroService.java
+++ src/main/java/com/carros/domain/CarroService.java
@@ -1,10 +1,15 @@
 package com.carros.domain;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import com.carros.domain.dto.CarroDTO;
 
 
 @Service
@@ -13,33 +18,68 @@ public class CarroService {
 	@Autowired
 	private CarroRepository rep;
 	
-	public Iterable<Carro> getCarros(){
-		return rep.findAll();
+	public List<CarroDTO> getCarros(){
+		
+			
+		return rep.findAll().stream().map(CarroDTO::new).collect(Collectors.toList());
+		
 	}
 	
 	public Optional<Carro> getCarroById(Long id) {
 		return rep.findById(id);
 	}
 	
-	public Iterable<Carro> getCarrosByTipo(String tipo) {
-		return rep.findByTipo(tipo);
+	public List<CarroDTO> getCarrosByTipo(String tipo) {
+		return rep.findByTipo(tipo).stream().map(CarroDTO::new).collect(Collectors.toList());
+		
 	}
 	
-	public Carro save(Carro carro) {
+	public Carro save(Carro carro) {	
 		return rep.save(carro);
 		
 	}
+	public Carro update(Carro carro, Long id) {
+		Assert.notNull(id, "Não foi possível atualizar o registro");
+		
+		Optional<Carro> optional = getCarroById(id);
+		if(optional.isPresent()) {
+			Carro db = optional.get();
+			db.setNome(carro.getNome());
+			db.setTipo(carro.getTipo());
+			System.out.println("Carro id" + db.getId());
+			
+			rep.save(db);
+			return db;
+	
+		}else{
+			throw new RuntimeException("Não foi possível atualizar o registro");
+		}
+		
+		
+	}
+	
+	public void delete(Long id) {
+		Optional<Carro> carro = getCarroById(id);
+		if(carro.isPresent()) {
+		rep.deleteById(id);
+		}
+		
+	}
+	
+	
+	
 	
 	public List<Carro> getCarrosFake(){
 		List<Carro> carros = new ArrayList<>();
 			
-		carros.add(new Carro(1L, "Fusca"));
-		carros.add(new Carro(2L, "Brasilia"));
-		carros.add(new Carro(3L, "Chevette"));
+	
 		
 		return carros;
 				
 	}
+
+	
+	
 
 
 
